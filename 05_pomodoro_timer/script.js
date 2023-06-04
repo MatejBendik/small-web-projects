7c386971a192cb3e4a98df668b82e4654c74d14e
MatejBendik <matejbendik.mb@gmail.com>
2023-06-04 12:17:04 +0200

    Add 5. project (pomodoro timer)

---
+++ 05_pomodoro_timer/script.js
@@ -0,0 +1,45 @@
+const startEl = document.querySelector("#start");
+const stopEl = document.querySelector("#stop");
+const resetEl = document.querySelector("#reset");
+const timerEl = document.querySelector("#timer");
+
+let interval;
+let timeLeft = 1500;
+
+function updateTimer() {
+  let minutes = Math.floor(timeLeft / 60);
+  let seconds = timeLeft % 60;
+
+  let formattedTime = `${minutes.toString().padStart(2, "0")}:${seconds
+    .toString()
+    .padStart(2, "0")}`;
+  timerEl.innerHTML = formattedTime;
+}
+
+function startTimer() {
+  interval = setInterval(() => {
+    timeLeft--;
+    updateTimer();
+
+    if (timeLeft === 0) {
+      clearInterval(interval);
+      alert("Time's up!");
+      timeLeft = 1500;
+      updateTimer();
+    }
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(interval);
+}
+
+function resetTimer() {
+  clearInterval(interval);
+  timeLeft = 1500;
+  updateTimer();
+}
+
+startEl.addEventListener("click", startTimer);
+stopEl.addEventListener("click", stopTimer);
+resetEl.addEventListener("click", resetTimer);
