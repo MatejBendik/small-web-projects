feb9caced79ec8d6dc09af1b052a3f42df7b0b8d
MatejBendik <matejbendik.mb@gmail.com>
2023-06-04 11:45:11 +0200

    Add 4. project (dice roll simulator)

---
+++ 04_dice_roll_simulator/script.js
@@ -0,0 +1,52 @@
+const buttonEl = document.querySelector("#roll-button");
+const diceEl = document.querySelector("#dice");
+const rollHistoryEl = document.querySelector("#roll-history");
+
+let historyList = [];
+
+function rollDice() {
+  const rollResult = Math.floor(Math.random() * 6) + 1;
+  const diceFace = getDiceFace(rollResult);
+  diceEl.innerHTML = diceFace;
+  historyList.push(rollResult);
+  updateRollHistory();
+}
+
+function updateRollHistory() {
+  rollHistoryEl.innerHTML = "";
+  for (let i = 0; i < historyList.length; i++) {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `Roll ${i + 1}: <span>${getDiceFace(
+      historyList[i]
+    )}</span>`;
+
+    rollHistoryEl.appendChild(listItem);
+  }
+}
+
+function getDiceFace(rollResult) {
+  switch (rollResult) {
+    case 1:
+      return "&#9856;";
+    case 2:
+      return "&#9857;";
+    case 3:
+      return "&#9858;";
+    case 4:
+      return "&#9859;";
+    case 5:
+      return "&#9860;";
+    case 6:
+      return "&#9861;";
+    default:
+      return "";
+  }
+}
+
+buttonEl.addEventListener("click", () => {
+  diceEl.classList.add("roll-animation");
+  setTimeout(() => {
+    diceEl.classList.remove("roll-animation");
+    rollDice();
+  }, 1000);
+});
